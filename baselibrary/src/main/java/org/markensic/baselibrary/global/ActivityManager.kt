f989fd056eb8f3434728316917d162237fe2fc32
maijunxian <markensic@outlook.com>
2020-11-25 19:46:09 +0800

    增加Activity栈，增加按步数回退Activity

--- baselibrary/src/main/java/org/markensic/baselibrary/global/ActivityManager.kt
+++ baselibrary/src/main/java/org/markensic/baselibrary/global/ActivityManager.kt
@@ -4,17 +4,22 @@ import android.app.Activity
 import android.os.Bundle
 import org.markensic.baselibrary.impl.ActivityLifecycle
 import java.lang.ref.WeakReference
+import java.util.*
 
 object ActivityManager: ActivityLifecycle {
+    val stack: Stack<WeakReference<Activity>> = Stack()
+
     var weakCurrentActivity: WeakReference<Activity>? = null
 
     var weakLasterActivity: WeakReference<Activity>? = null
 
     override fun onActivityCreated(p0: Activity, savedInstanceState: Bundle?) {
         super.onActivityCreated(p0, savedInstanceState)
+        stack.push(WeakReference(p0))
+
         weakLasterActivity.also {
             it?.clear()
-            weakLasterActivity = WeakReference<Activity>(p0)
+            weakLasterActivity = WeakReference(p0)
         }
     }
 
@@ -28,6 +33,21 @@ object ActivityManager: ActivityLifecycle {
         weakLasterActivity.also {
             it?.clear()
             weakLasterActivity = WeakReference<Activity>(p0)
+        }
+    }
+
+    override fun onActivityDestroyed(p0: Activity) {
+        super.onActivityDestroyed(p0)
+        stack.pop()
+    }
+
+    fun backToStep(step: Int) {
+        stack.mapNotNull {
+            it.get()
+        }.apply {
+            for (i in size - 1 downTo step + 1) {
+                this[i].finish()
+            }
         }
     }
 }
